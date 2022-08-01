2f683a0026d687ded7f769d27076d7dead1db1fd
Cristian Cortes Mondragon <ccortesm2000@gmail.com>
2022-07-31 23:31:40 -0500

    models user

---
+++ main.py
@@ -0,0 +1,51 @@
+#Python
+from datetime import date
+from typing import Optional
+from uuid import UUID
+
+
+#Pydantic
+from pydantic import BaseModel
+from pydantic import EmailStr
+from pydantic import Field
+
+#FastAPI 
+from fastapi import FastAPI
+
+app = FastAPI()
+
+# Models
+class UserBase(BaseModel):
+    user_id: UUID = Field(...)
+    email: EmailStr = Field(...)
+
+
+class UserLogin(UserBase):
+    password: str = Field(
+        ...,
+        min_length=8
+    )
+
+
+class user(UserBase):
+
+    first_name: str = Field(
+        ...,
+        min_length=1,
+        max_length=50
+    )
+    last_name= str =Field(
+        ...,
+        min_length=1,
+        max_length=50
+    )
+    birth_date = Optional[date] = Field(default=None)
+
+
+class tweet(BaseModel):
+    pass
+
+
+@app.get(path="/")
+def home():
+    return {"Twitter API":"Working!"}
